85b4a10e79df18bbe4c23d42c34ad9de610a945a
Mikko Lyly <mikko.lyly@outlook.com>
2022-12-30 19:36:00 +0200

    Update example_1.ino

--- examples/example_1.ino
+++ examples/example_1.ino
@@ -2,13 +2,13 @@
 //            Simple time-sliced multithreading on Arduino UNO
 //                [see time_slicing.ino for more details]
 //
-// Task 1 handles LED blinking
-// Task 2 handles serial communication
+// Task 0 handles LED blinking
+// Task 1 handles serial communication
 //
 // Written by: Mikko Lyly
 // Date:       29 dec 2022
 //------------------------------------------------------------------------------
-#define pushAddress(task) asm("push %A0 \n push %B0" :: "x" (task));
+#define pushAddress(task) asm("push %A0\n push %B0" :: "x" (task));
 
 #define pushContext() asm("push r0\n in r0, 0x3f\n push r0\n push r1\n"        \
 "clr r1\n push r2\n push r3\n push r4\n push r5\n push r6\n push r7\n"         \
@@ -29,11 +29,11 @@ volatile uint8_t currentTask = 0;
 volatile uint32_t currentTicks = 0;
 volatile bool ledStatus = false;
 
-void task_1() {
+void task_0() {
   Serial.println(F("LED blinker starting"));
   
   for(;;) {
-    while(ledStatus == false) {}
+    while(ledStatus == false);
     digitalWrite(13, HIGH);
     delay(250);
     digitalWrite(13, LOW);
@@ -41,32 +41,31 @@ void task_1() {
   }
 }
 
-void task_2() {
+void task_1() {
   Serial.println(F("Serial console starting"));
   Serial.println(F("Type 'on' or 'off' to start or stop blinking"));
   String str = "";
-
-  for(;;) {        
-    while(Serial.available() > 0) {
-      char c = Serial.read();
-      switch(c) {
-      case '\n':
-        if(str == "on") {
-          Serial.println(F("Blinking ON"));
-          ledStatus = true;
-        } else if(str == "off") {
-          Serial.println(F("Blinking OFF"));
-          ledStatus = false; 
-        } else {
-          Serial.println(F("Unknown command"));
-        }
-        str = "";
-        break;
-      case '\r':
-        break;
-      default:
-        str += c;
+  
+  for(;;) {
+    while(Serial.available() == 0);
+    char c = Serial.read();
+    switch(c) {
+    case '\r':
+      break;
+    case '\n':
+      if(str == "on") {
+        Serial.println(F("Blinking ON"));
+        ledStatus = true;
+      } else if(str == "off") {
+        Serial.println(F("Blinking OFF"));
+        ledStatus = false; 
+      } else {
+        Serial.println(F("Unknown command"));
       }
+      str = "";
+      break;
+    default:
+      str += c;
     }
   }
 }
@@ -78,23 +77,23 @@ void setup() {
   Serial.println(F("Initializing"));
 
   SP = stackPointers[0];
-  pushAddress(task_1);
+  pushAddress(task_0);
   pushContext();
   stackPointers[0] = SP;
 
   SP = stackPointers[1];
-  pushAddress(task_2);
+  pushAddress(task_1);
   pushContext();
   stackPointers[1] = SP;
-  
+
   cli();
   TCCR2A = _BV(WGM21);
   TCCR2B = _BV(CS22);
   OCR2A = 249;
   TIMSK2 |= _BV(OCIE2A);
   sei();
-
-  SP = stackPointers[currentTask];
+  
+  SP = stackPointers[0];
   popContext();
   asm("ret");
 }
